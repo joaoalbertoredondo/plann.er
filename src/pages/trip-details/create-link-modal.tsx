37432e32ee94e6c0112ce67eb98ac87f613359c7
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2024-07-11 13:15:55 -0300

    links functionality

--- src/pages/trip-details/create-link-modal.tsx
+++ src/pages/trip-details/create-link-modal.tsx
@@ -1,32 +1,32 @@
-import { Calendar, Tag, X } from "lucide-react";
+import { Link2, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
 import { FormEvent } from "react";
 import { api } from "../../lib/axios";
-import { useParams } from "react-router-dom";
 
-interface CreateActivityModalProps {
-  closeCreateActivityModal: () => void;
+interface CreateLinkModalProps {
+  closeCreateLinkModal: () => void;
 }
 
-export function CreateActivityModal({
-  closeCreateActivityModal,
-}: CreateActivityModalProps) {
+export function CreateLinkModal({
+  closeCreateLinkModal,
+}: CreateLinkModalProps) {
   const { tripId } = useParams();
 
-  async function createActivity(event: FormEvent<HTMLFormElement>) {
+  async function createLink(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const data = new FormData(event.currentTarget);
 
     const title = data.get("title")?.toString();
-    const occurs_at = data.get("occurs_at")?.toString();
+    const url = data.get("urlInput")?.toString();
 
-    await api.post(`/trips/${tripId}/activities`, {
+    await api.post(`/trips/${tripId}/links`, {
       title,
-      occurs_at,
+      url,
     });
 
-    window.document.location.reload()
+    window.document.location.reload();
   }
 
   return (
@@ -34,38 +34,39 @@ export function CreateActivityModal({
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Cadastrar atividade </h2>
-            <button type="button" onClick={closeCreateActivityModal}>
+            <h2 className="text-lg font-semibold">Cadastrar Link</h2>
+            <button type="button" onClick={closeCreateLinkModal}>
               <X className="size-5 text-zinc-400" />
             </button>
           </div>
           <p className="text-sm text-zinc-400">
-            Todos convidados podem visualizar as atividades.
+            Todos convidados podem visualizar os links importantes.
           </p>
         </div>
 
-        <form onSubmit={createActivity} className="space-y-3">
+        <form onSubmit={createLink} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-            <Tag className="zise-5 text-zinc-400" />
+            <Tag className="size-5 text-zinc-400" />
             <input
+              type="text"
               name="title"
-              placeholder="Qual a atividade?"
+              placeholder="Título do link"
               className="bg-transparent text-lg placeholder:text-zinc-400 outline-none flex-1"
             />
           </div>
 
-          <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-            <Calendar className="zise-5 text-zinc-400" />
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Link2 className="size-5 text-zinc-400" />
             <input
-              type="datetime-local"
-              name="occurs_at"
-              placeholder="Data e horário da atividade"
+              type="text"
+              name="urlInput"
+              placeholder="URL"
               className="bg-transparent text-lg placeholder:text-zinc-400 outline-none flex-1"
             />
           </div>
 
           <Button variant="primary" size="full">
-            Salvar atividade
+            Salvar link
           </Button>
         </form>
       </div>
--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,45 +1,68 @@
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { useEffect, useState } from "react";
+import { CreateLinkModal } from "./create-link-modal";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+
+interface Link {
+  id: string;
+  title: string;
+  url: string;
+};
 
 export function ImportantLinks() {
+  const { tripId } = useParams();
+  const [links, setLinks] = useState<Link[]>([]);
+
+  const [isCreateLinkModalOpen, setIsCreateLinkModalOpen] = useState(false);
+
+  function openCreateLinkModal() {
+    setIsCreateLinkModalOpen(true);
+  }
+
+  function closeCreateLinkModal() {
+    setIsCreateLinkModalOpen(false);
+  }
+
+  useEffect(() => {
+    api.get(`/trips/${tripId}/links`).then((response) => {
+      setLinks(response.data.links);
+    });
+  }, [tripId]);
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
       <div className="space-y-5">
-        <div className="flex items justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/104700011u734y98ry239r394
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
-        <div className="flex items justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">
-              Reserva do AirBnB
-            </span>
-            <a
-              href="#"
-              className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
-            >
-              https://www.airbnb.com.br/rooms/104700011u734y98ry239r394
-            </a>
-          </div>
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+        {links.map((link) => {
+          return (
+            <div key={link.id} className="flex items justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">
+                  {link.title}
+                </span>
+                <a
+                  href="#"
+                  className="block text-xs text-zinc-400 truncate hover:text-zinc-200"
+                >
+                  {link.url}
+                </a>
+              </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
+          );
+        })}
       </div>
 
-      <Button variant="secondary" size="full">
+      <Button variant="secondary" size="full" onClick={openCreateLinkModal}>
         <Plus className="size-5" />
         Cadastrar novo link
       </Button>
+
+      {isCreateLinkModalOpen && (
+        <CreateLinkModal closeCreateLinkModal={closeCreateLinkModal} />
+      )}
     </div>
   );
 }
